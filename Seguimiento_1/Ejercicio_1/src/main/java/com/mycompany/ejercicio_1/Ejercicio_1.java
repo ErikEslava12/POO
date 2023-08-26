1033068bdbdd296afd5e337fdf425b250fabd036
ErikEslava12 <69918613+ErikEslava12@users.noreply.github.com>
2023-08-26 09:30:43 -0500

    Change_1

---
+++ Seguimiento_1/Ejercicio_1/src/main/java/com/mycompany/ejercicio_1/Ejercicio_1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_1;
+import java.util.Scanner;
+
+/**
+ *
+ * @author erike
+ */
+public class Ejercicio_1 {
+
+    public static void main(String[] args) {
+        int EDJUAN;
+        int EDALBER;
+        int EDANA;
+        int EDMAMA;
+        Scanner Entrada = new Scanner(System.in);
+        System.out.println("Ingrese la edad de Juan: ");
+        EDJUAN = Entrada.nextInt();
+        EDALBER = 2*EDJUAN/3;
+        EDANA = 4*EDJUAN/3;
+        EDMAMA = EDJUAN + EDALBER + EDANA;
+        System.out.println("La edad de todos es: "+"La edad de la mamá:"+EDMAMA);
+        System.out.println("La edad de Alber:"+EDALBER);
+        System.out.println("La edad de Ana:"+EDANA);
+        
+    }
+}
